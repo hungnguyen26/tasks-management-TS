31dce732a04cd44f83d02b4e8371d5d197d0edc3
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-06-28 23:43:49 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("ok 22");
+import express,{ Express , Request , Response} from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/tasks", (req: Request, res: Response) => {
+  res.send("Danh sách công việc");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
