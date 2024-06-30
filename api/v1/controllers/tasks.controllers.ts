1a2f9ded82716dc801ad13bf20c7b092606e3574
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2024-07-01 00:30:19 +0700

    Sắp xếp theo tiêu chí

--- api/v1/controllers/tasks.controllers.ts
+++ api/v1/controllers/tasks.controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Task from "../model/tasks.model";
 
 export const index = async (req: Request, res: Response) => {
+  //find
   const find = {
     deleted: false,
   }
@@ -9,9 +10,20 @@ export const index = async (req: Request, res: Response) => {
   if(req.query.status){
     find["status"] = req.query.status;
   }
+  //end find
 
-  const tasks = await Task.find(find);
-  
+  //sort
+  const sort = {
+
+  }
+  if(req.query.sortKey && req.query.sortValue){
+    const sortKey = req.query.sortKey.toLocaleString();
+    sort[sortKey] = req.query.sortValue;
+  }
+  //end sort
+
+  const tasks = await Task.find(find).sort(sort);
+
   res.json(tasks);
 };
 
